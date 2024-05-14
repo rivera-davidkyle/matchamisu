ee6edac37718b5c468b08bca41f18df10e043863
rivera-davidkyle <rivera.davidkyle@gmail.com>
2024-05-14 13:42:46 -0400

    Added DnD and click to upload image

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Recipe from "./components/recipe.jsx";
 import { Container, Grid } from "@mui/material";
 import React, { useEffect, useState, useCallback } from "react";
 import recipeStruct from "./static/recipe_structure.json";
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GOOGLE_AI_API_KEY);
 
 // Function to convert image to base64
 function convertBlobToBase64(file) {
@@ -29,40 +28,32 @@ function validateJSON(jsonString) {
   }
 }
 
-let secondaryStyle = {
-
-}
-
 function App() {
   const [jsonObject, setJsonObject] = useState();
-  const [selectedFile, setSelectedFile] = useState();
 
   const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
   const prompt = `Create a recipe of this image following this schema ${JSON.stringify(
     recipeStruct
   )}. Don't format in markdown, but just a JSON string.`;
 
-  // const handleFileChange = (item) => {
-  //   setSelectedFile(item.files[0]);
-  //   getRecipe();
-  // };
-  const handleFileChange = useCallback(
-    (item) => {
-      if (item) {
-        const files = item.files[0];
-        setSelectedFile(files)
-        getRecipe();
-      }
-    },
-    [setSelectedFile],
-  );
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    getRecipe(file);
+  };
 
-  async function getRecipe() {
+  const handleFileDrop = useCallback((item) => {
+    if (item) {
+      const file = item.files[0];
+      getRecipe(file);
+    }
+  }, []);
+
+  async function getRecipe(files) {
     let jsonString = "";
     console.log("Getting recipe..");
     try {
       while (!validateJSON(jsonString)) {
-        const base64String = await convertBlobToBase64(selectedFile);
+        const base64String = await convertBlobToBase64(files);
         const image = {
           inlineData: {
             data: base64String,
@@ -74,20 +65,20 @@ function App() {
         const response = await result.response;
         jsonString = response.text();
       }
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
     setJsonObject(JSON.parse(jsonString));
     console.log(jsonString);
   }
-  
+
   return (
     <div className="App">
       <Container disableGutters>
         <Grid container spacing={4}>
           <Grid item xs={12}>
             <DndProvider backend={HTML5Backend}>
-              <Upload onDrop={handleFileChange}/>
+              <Upload onDrop={handleFileDrop} onFileChange={handleFileChange} />
             </DndProvider>
           </Grid>
           <Grid item xs={12}>
--- src/components/upload.jsx
+++ src/components/upload.jsx
@@ -1,46 +1,54 @@
-import React from 'react'
-import { Paper } from "@mui/material";
-import { useDrop } from 'react-dnd'
-import { NativeTypes } from 'react-dnd-html5-backend'
+import React from "react";
+import { Paper, Button } from "@mui/material";
+import { useDrop } from "react-dnd";
+import { NativeTypes } from "react-dnd-html5-backend";
 
+let initialStyle = {
+  border: "5px dashed",
+  borderColor: "#4A490F",
+  background: "#D8D796",
+  display: "block",
+};
 const UploadZone = (props) => {
-    const { onDrop } = props
-    const [{ canDrop, isOver }, drop] = useDrop(
-        () => ({
-        accept: [NativeTypes.FILE],
-        drop(item) {
-            if (onDrop) {
-            onDrop(item)
-            }
-        },
-        canDrop(item) {
-            console.log('canDrop', item.files, item.items)
-            return true
-        },
-        hover(item) {
-            console.log('hover', item.files, item.items)
-        },
-        collect: (monitor) => {
-            const item = monitor.getItem()
-            if (item) {
-            console.log('collect', item.files, item.items)
-            }
-            return {
-            isOver: monitor.isOver(),
-            canDrop: monitor.canDrop(),
-            }
-        },
-        }),
-        [props],
-    )
-  
-    const isActive = canDrop && isOver;
-  
-    return (
-        <Paper ref={drop}>
-            {isActive ? 'Release to drop' : 'Drag file here'}
-        </Paper>
-    );
-  };
-  
-  export default UploadZone;
+  const { onDrop, onFileChange } = props;
+  const [{ canDrop, isOver }, drop] = useDrop(
+    () => ({
+      accept: [NativeTypes.FILE],
+      drop(item) {
+        if (onDrop) {
+          onDrop(item);
+        }
+      },
+      collect: (monitor) => {
+        const item = monitor.getItem();
+        return {
+          isOver: monitor.isOver(),
+          canDrop: monitor.canDrop(),
+        };
+      },
+    }),
+    [props]
+  );
+
+  const isActive = canDrop && isOver;
+
+  return (
+    <Paper ref={drop} sx={initialStyle}>
+      {isActive ? "Release to drop" : "Drag file here"}
+      <input
+        type="file"
+        accept="image/*"
+        onChange={onFileChange}
+        id="raised-button-file"
+        hidden
+      />
+      <label htmlFor="raised-button-file">
+        <Button variant="raised" component="span">
+          Upload
+        </Button>
+      </label>
+    </Paper>
+  );
+};
+
+export default UploadZone;
